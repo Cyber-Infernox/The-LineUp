f1dad432f29aba202fb452ab307f3e8509e7dc66
Cyber_Infernox <sayonkar@gmail.com>
2022-11-29 09:59:41 +0530

    Updated Navbar

---
+++ the-lineup/src/Navbar.js
@@ -0,0 +1,24 @@
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="flex justify-between mx-96 mt-4 mb-3">
+        <h1 className="text-2xl text-blue-700 pt-1">The LineUp__</h1>
+
+        <ul className="flex text-lg">
+          <li className="py-1 px-2 rounded-lg text-gray-500 cursor-pointer hover:text-black">
+            Pending
+          </li>
+          <li className="py-1 px-2 ml-4 rounded-lg text-gray-500 cursor-pointer hover:text-black">
+            Completed
+          </li>
+          <li className="bg-blue-600 text-white ml-6 py-1 px-2 border rounded-lg cursor-pointer hover:text-gray-700">
+            New
+          </li>
+        </ul>
+      </nav>
+      <hr className="mx-96" />
+    </div>
+  );
+};
+
+export default Navbar;
