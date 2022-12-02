52c7d3e36e606502acfb5c33eb650f9c13c624bd
Cyber_Infernox <sayonkar@gmail.com>
2022-12-02 09:02:28 +0530

    Fixed completed and pending tasks

--- the-lineup/src/TaskDetails.js
+++ the-lineup/src/TaskDetails.js
@@ -10,6 +10,36 @@ const TaskDetails = () => {
   } = useFetch("http://localhost:8000/tasks/" + id);
   const navigate = useNavigate();
 
+  const updateData = () => {
+    fetch("http://localhost:8000/tasks/" + task.id, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: task.title,
+        body: task.body,
+        deadline: task.deadline,
+        Checked: true,
+      }),
+    }).then(() => {
+      navigate("/");
+    });
+  };
+
+  const updateBackData = () => {
+    fetch("http://localhost:8000/tasks/" + task.id, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: task.title,
+        body: task.body,
+        deadline: task.deadline,
+        Checked: false,
+      }),
+    }).then(() => {
+      navigate("/");
+    });
+  };
+
   const handleClick = () => {
     fetch("http://localhost:8000/tasks/" + task.id, {
       method: "DELETE",
@@ -23,18 +53,38 @@ const TaskDetails = () => {
       {isPending && <div>Loading...</div>}
       {error && <div>{error}</div>}
       {task && (
-        <article>
-          <div className="font-bold">
-            <h2 className="text-2xl mb-2">{task.title}</h2>
-            <p className="mb-4">Submit by {task.deadline}</p>
+        <article className="pb-2">
+          <div>
+            <div className="font-bold">
+              <h2 className="text-2xl mb-2">{task.title}</h2>
+              <p className="mb-4">Submit by {task.deadline}</p>
+            </div>
+            <div className="pl-12 pt-2 pb-6">{task.body}</div>
           </div>
-          <div className="pl-12 pt-2 pb-6">{task.body}</div>
-          <button
-            className="bg-blue-500 ml-1 hover:text-gray-500 text-white py-1 px-2 rounded-lg"
-            onClick={handleClick}
-          >
-            Delete
-          </button>
+          <div className="flex items-center">
+            {!task.Checked && (
+              <button
+                onClick={updateData}
+                className="bg-blue-500 ml-1 hover:text-gray-500 text-white px-2 py-1 mr-8 rounded-lg"
+              >
+                Completed
+              </button>
+            )}
+            {task.Checked && (
+              <button
+                onClick={updateBackData}
+                className="bg-blue-500 ml-1 hover:text-gray-500 text-white px-2 py-1 mr-8 rounded-lg"
+              >
+                Pending
+              </button>
+            )}
+            <button
+              className="bg-blue-500 ml-1 hover:text-gray-500 text-white py-1 px-2 rounded-lg"
+              onClick={handleClick}
+            >
+              Delete
+            </button>
+          </div>
         </article>
       )}
     </div>
--- the-lineup/src/TaskList.js
+++ the-lineup/src/TaskList.js
@@ -13,27 +13,13 @@ const TaskList = ({ tasks, title }) => {
         {tasks.map(
           (task) =>
             !task.Checked && (
-              <div
-                className="p-2 hover:shadow pl-1 flex justify-between items-center mb-1"
-                key={task.id}
-              >
+              <div className="p-2 hover:shadow pl-1 mb-1" key={task.id}>
                 <Link to={`/tasks/${task.id}`}>
                   <div className="p-3">
                     <h2 className="font-bold text-lg pb-1">{task.title}</h2>
                     <p>Deadline: {task.deadline}</p>
                   </div>
                 </Link>
-                <div>
-                  <button
-                    onClick={() => {
-                      task.Checked = true;
-                      navigate("/");
-                    }}
-                    className="bg-blue-500 hover:text-gray-500 text-white px-2 py-2 mr-3 rounded-xl"
-                  >
-                    Completed
-                  </button>
-                </div>
               </div>
             )
         )}
